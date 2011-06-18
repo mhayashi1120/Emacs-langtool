bc4bc3cfbe6c06611a4ea930744f319d5f16d268
Hayashi Masahiro <mhayashi1120@gmail.com>
2011-06-18 22:11:19 +0900

    remove emacswiki url

--- langtool.el
+++ langtool.el
@@ -3,7 +3,6 @@
 ;; Author: Masahiro Hayashi <mhayashi1120@gmail.com>
 ;; Keywords: grammer checker java
 ;; URL: http://github.com/mhayashi1120/Emacs-langtool/raw/master/langtool.el
-;; URL: http://www.emacswiki.org/emacs/download/langtool.el
 ;; Emacs: GNU Emacs 22 or later
 ;; Version: 1.0.1
 
